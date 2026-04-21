fae44e46bd589d9cb7a1d44ca65b24b289902fb7
Jaemi <135385091+jaemicrac@users.noreply.github.com>
2026-04-21 12:42:49 -0400

    Update project3.js

--- js/project3.js
+++ js/project3.js
@@ -20,6 +20,23 @@ var timerDisplay = document.getElementById("timer");
 var countdown = setInterval(function() {
 	var minutes = Math.floor(timeLimit / 60);
 	var seconds = timeLimit % 60;
+
 	if (seconds < 10) {
 		seconds = "0" +seconds;
 	}
+
+	// To update time remaining on the screen 
+	timerDisplay.innerHTML = minutes + ";" + seconds;
+
+	// For when the timer runs out 
+	if (timeLimit <= 0) {
+		clearInterval(countdown); 
+		alert("Sorry, your time to complete the form has expired! Please try again if you still wish to purchase tickets."); [cite: 6]
+		location.href = "event_registration.html"; // To redirect back to same page
+	}
+
+		timeLimit--;
+}, 1000);
+	
+
+	
