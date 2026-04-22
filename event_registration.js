5bc92474c571b6f808579c0786a9b33f72460a31
Jaemi <135385091+jaemicrac@users.noreply.github.com>
2026-04-22 19:38:17 -0400

    Update event_registration.js

--- event_registration.js
+++ event_registration.js
@@ -40,3 +40,32 @@ if (timeRemaining <= 0) {
 
 timeRemaining--; //Decreases by 1 second at each interval
 }, 1000); 
+
+//To calculate total number of tickets 
+function calculationTotal() {
+	var numTicketsField = document.getElementById("numTickets");
+	var numTickets = numTicketsField.value;
+	var errorSpan = document.getElementById("msgTickets");
+	var contactSection = document.getElementById("contactInformation");
+	var totalCostField = document.getElementById("totalCost);
+
+//Validates and checks if input is a number or outside of the 1-3 range
+if (isNaN(numTickets) || numTickets < minTickets || numTickets > maxTickets || numTickets === "") {
+	//Shows an error message and turns the field to yellow
+	error.Span.innerHTML = "You can only buy between 1 and 3 tickets."; 
+	numTicketsField.style.backgroundColor = "yellow";
+	contactSection.style.display = "none"; //To keep contact information hidden
+	totalCostField.value = $0.00";
+} else {
+	errorSpan.innerHTML = "";
+	numTicketsField.style.backgroundColor = "white"
+
+//Calculation for tickets * ticket price + surcharge
+var total = (parseInt(numTickets) * costPerTicket) + ticketSurcharge;
+
+//Format to fixed 2 decimal places with &
+totalCostField.value = "$" + total.toFixed(2); 
+
+//Reveal the contact section 
+contactSection.style.display = "block";
+}
